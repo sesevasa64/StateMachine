066824b5ca31526f8491195677f5d6baaf8dda11
sesevasa64@gmail.com <sesevasa64@gmail.com>
2020-05-10 19:13:36 +0500

    Added comment

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
     std::cout << st.fucking_work("0xff"s) << std::endl;
     std::cout << st.fucking_work("0o77"s) << std::endl;
     std::cout << st.fucking_work("0b101"s) << std::endl;
-    // 
+    // Константа ниже не распознается в с++, но этот автомат может ее распознать :)
     std::cout << st.fucking_work("0x.8"s) << std::endl;
     return 0;
 }
